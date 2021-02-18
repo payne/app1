96850a4981251fa769ffcab3951b8879dcc8f262
Matt Payne <Payne@MattPayne.org>
2021-02-18 16:51:46 -0600

    End of video #4

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from "react";
+import PropTypes from "prop-types";
 import './App.css';
 import pokemon from "./pokemon.json"
 
@@ -7,6 +9,15 @@ const PokemonRow = ({ pokemon }) => (
           <td>{pokemon.type.join(", ")}</td>
         </tr>
 );
+
+PokemonRow.propTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.shape({
+      english: PropTypes.string,
+    }),
+    type: PropTypes.arrayOf(PropTypes.string),
+  })
+};
 
 function App() {
   return (
