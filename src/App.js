458b65b767701880d52f3433ad8427b9ce9221a7
Matt Payne <Payne@MattPayne.org>
2021-02-18 16:39:41 -0600

    Following along with video #4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import './App.css';
 import pokemon from "./pokemon.json"
 
+const PokemonRow = ({ pokemon }) => (
+        <tr>
+          <td>{pokemon.name.english}</td>
+          <td>{pokemon.type.join(", ")}</td>
+        </tr>
+);
 
 function App() {
   return (
@@ -20,10 +26,8 @@ function App() {
       </thead>
       <tbody>
         {pokemon.slice(0,20).map((pokemon) => (
-        <tr key={pokemon.id}>
-          <td>{pokemon.name.english}</td>
-          <td>{pokemon.type.join(", ")}</td>
-        </tr>
+          <PokemonRow pokemon={pokemon} key={pokemon.id} />
+
         ))}
      </tbody>
     </table>
